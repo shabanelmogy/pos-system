d8f19a1745e1d2bdd6afb561d9dcfc3879899477
ShabanElmoogy <shaban.cec@gmail.com>
2026-05-13 16:05:18 +0300

    install shadcn

--- pos-frontend/vite.config.js
+++ pos-frontend/vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     basicSsl(),
   ],
-})
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
